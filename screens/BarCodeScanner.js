6d0db0017b93fa69b99e8efd066e8377fd906f6f
Rudolfinio <rafi321112@gmail.com>
2023-12-06 21:34:20 +0100

    skaner

---
+++ screens/BarCodeScanner.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect} from 'react';
+import { StyleSheet,View, Text, Button } from 'react-native';
+import { BarCodePoint, BarCodeScanner } from 'expo-barcode-scanner';
+
+
+// const BCScanner = ({ navigation }) =>
+export default function App() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [text, setText] = useState('Not yet scanned');
+
+    const askForCameraPermission = () => 
+    {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status == 'granted');
+        })()
+    }
+
+
+useEffect(()=> {
+    askForCameraPermission();
+}, []);
+
+const handleBarCodeScanned = ({type, data}) => {
+    setScanned(true);
+    setText(data);
+    console.log('Type: '+type + '\nData: '+data);
+}
+
+if(hasPermission === null){
+    return(
+
+        <View>
+            <Text>Requesting for camera permission</Text>
+        </View>
+    )
+}
+
+if(hasPermission === false){
+    return(
+        <View>
+            <Text>No access to camera</Text>
+            <Button title={'Allow Camera'} onPress={()=>askForCameraPermission()} />
+        </View>
+    )
+}
+
+ // Return the View
+ return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    maintext: {
+      fontSize: 16,
+      margin: 20,
+    },
+    barcodebox: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 300,
+      width: 300,
+      overflow: 'hidden',
+      borderRadius: 30,
+      backgroundColor: 'tomato'
+    }
+  });
---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      /> 
+      <Button
+      title="Go to Camera"
+      onPress={() => navigation.navigate('Camera')}
+    />
+    </View>
+  );
+};
+
+export default HomeScreen;
